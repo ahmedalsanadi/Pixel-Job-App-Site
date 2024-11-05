f5d285fe6cf194e333ad064f7f51cb9345a89999
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-05 23:09:32 +0300

    Feat : Implement Section Heading Component

---
+++ resources/views/components/section-heading.blade.php
@@ -0,0 +1,5 @@
+<!-- section-heading component -->
+<div class="inline-flex items-center gap-x-2">
+    <span class="w-2 h-2 bg-white inline-block"></span>
+    <h3 class="font-bold text-lg">{{ $slot }}</h3>
+</div>
