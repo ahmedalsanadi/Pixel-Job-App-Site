32c84ef7eec3d075e3ed961230c4e966b5b0352f
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-05 22:13:54 +0300

    Feat : Include Images folder for vite +  intiate layout.blade.php

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pixel Positions</title>
+    @Vite(['resources/js/app.js'])
+</head>
+<body>
+    <div>
+        <nav>
+            <div>
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="logo">
+                </a>
+            </div>
+            <div>
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+
+            </div>
+            <div>
+                <a href="#">Post a Job</a>
+            </div>
+        </nav>
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+
+</body>
+</html>
+
