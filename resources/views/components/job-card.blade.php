f2d2d825e6b2b55f90db34bf9175aeabeb69ebda
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-05 22:58:32 +0300

    Feat: Create Job Card component for displaying job listings

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Employer Name</div>
+    <div class="py-8 font-bold ">
+        <h3>Job Title</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <a href=""
+                class="bg-white/10 hover:bg-white/25  px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tags</a>
+            <a href=""
+                class="bg-white/10 hover:bg-white/25  px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tags</a>
+            <a href=""
+                class="bg-white/10 hover:bg-white/25  px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tags</a>
+        </div>
+        <img src="http://placehold.it/42/42" alt="Employer logo" class="rounded-xl">
+    </div>
+</div>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,15 +6,15 @@
     <title>Pixel Positions</title>
     @Vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
                 <a href="#">
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="logo">
                 </a>
             </div>
-            <div>
+            <div class="space-x-6 font-bold">
                 <a href="#">Jobs</a>
                 <a href="#">Careers</a>
                 <a href="#">Salaries</a>
@@ -25,7 +25,7 @@
                 <a href="#">Post a Job</a>
             </div>
         </nav>
-        <main>
+        <main class="mt-10 max-w-[986px] mx-auto">
             {{ $slot }}
         </main>
     </div>
