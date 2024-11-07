fab5e6c9acb60a3739a963dae6e2027f40fa6d17
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-07 03:05:25 +0300

    Feat: Implement user registration and employer creation
    - Created the `store` method in `RegisteredUserController` to handle user registration:
      - Validates user input for `name`, `email`, and `password`, ensuring required fields and uniqueness of email.
      - Validates employer input for `employer name` and `logo`, enforcing required fields and file type restrictions.
      - Creates a new `User` instance with validated attributes.
      - Stores the uploaded logo in the `logos` directory and saves the path.
      - Creates an associated `Employer` instance linked to the newly registered user with the provided employer name and logo path.
      - Automatically logs in the user after registration.
      - Redirects the user to the home page upon successful registration.

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,63 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employer;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\ImageFile;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+use Auth;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
 
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view("auth.register");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        // dd($request->all());
+        $userAttributes = $request->validate([
+            "name" => ["required", "string"],
+            "email" => ["required", "email", " unique:users,email"], // unique in users table specifically on email column
+            "password" => ["required", "confirmed", Password::min(6)],
+        ]);
+
+
+        $employerAttributes = $request->validate([
+            "employer" => ["required", "string"],
+            "logo" => ["required", ImageFile::types(["png", "jpg", "jpeg", "webp", "svg"])->max(2048)],
+        ]);
+
+
+        $user = User::create($userAttributes);
+        $logoPath = $request->logo->store("logos"); // store logo on logos folder
+
+        // store employer with logo_path
+        $user->employer()->create([
+            "name" => $employerAttributes["employer"],
+            "logo" => $logoPath
+        ]);
+
+        Auth::login($user);
+
+        return redirect("/");
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
