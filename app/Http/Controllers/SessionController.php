62ea35c6c9cdb9277cdedfab29f36a79189537f2
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-07 03:20:15 +0300

    Feat: Implement user login functionality in SessionController
    - Added the `store` method to handle user login:
      - Validates user input for `email` and `password`, ensuring both fields are required.
      - Uses `Auth::attempt()` to authenticate the user with the provided credentials.
      - Throws a `ValidationException` with a custom error message if authentication fails.
      - Regenerates the session to prevent session fixation attacks upon successful login.
      - Redirects the user to the home page upon successful authentication.

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,23 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
-
-
     public function create()
     {
-        return view("auth.login");
+        return view('auth.login');
     }
 
-
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (! Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, those credentials do not match.',
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect('/');
     }
-
-
-
 }
