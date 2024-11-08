2456db17bf7f3aaf6fa1588ab215dded32b91688
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-08 04:40:29 +0300

    Feat: Added real logo urls for job factory

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -5,21 +5,58 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employer>
- */
 class EmployerFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $companies = [
+        [
+            'name' => 'Google',
+            'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png'
+        ],
+        [
+            'name' => 'Microsoft',
+            'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png'
+        ],
+        [
+            'name' => 'Apple',
+            'logo' => 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg'
+        ],
+        [
+            'name' => 'Amazon',
+            'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png'
+        ],
+        [
+            'name' => 'Meta',
+            'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Meta_Platforms_Inc._logo.svg/2560px-Meta_Platforms_Inc._logo.svg.png'
+        ],
+        [
+            'name' => 'Netflix',
+            'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png'
+        ],
+        [
+            'name' => 'Tesla',
+            'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Tesla_Motors.svg/2560px-Tesla_Motors.svg.png'
+        ],
+        [
+            'name' => 'IBM',
+            'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IBM_logo.svg/2560px-IBM_logo.svg.png'
+        ],
+        [
+            'name' => 'Intel',
+            'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Intel_logo_%282006-2020%29.svg/1280px-Intel_logo_%282006-2020%29.svg.png'
+        ],
+        [
+            'name' => 'Adobe',
+            'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Adobe_Corporate_Logo.png/1280px-Adobe_Corporate_Logo.png'
+        ]
+    ];
+
     public function definition(): array
     {
+        $company = $this->companies[array_rand($this->companies)];
+
         return [
-            'name' => fake()->company(),
-            'logo' => fake()->imageUrl(),
+            'name' => $company['name'],
+            'logo' => $company['logo'],
             'user_id' => User::factory(),
         ];
     }
